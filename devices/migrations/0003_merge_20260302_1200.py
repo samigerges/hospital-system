9f7679bae5c9377af31527ebdd21516654c1b6a4
Samy Gerges <100544736+samigerges@users.noreply.github.com>
2026-03-02 13:44:44 +0200

    Resolve devices migration conflict with merge migration

---
+++ devices/migrations/0003_merge_20260302_1200.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("devices", "0002_maintenance_assigned_technician_and_more"),
+        ("devices", "0002_pmtemplate_maintenancetask"),
+    ]
+
+    operations = []
